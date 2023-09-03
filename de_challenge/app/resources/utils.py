cbc16ab5f3430ca1d2ea1e1bd33b1d7b3c568734
Bruno Tomas Caracini <bruno98980@gmail.com>
2023-09-03 02:11:12 -0300

    Utils class added

---
+++ de_challenge/app/resources/utils.py
@@ -0,0 +1,22 @@
+class Utils:
+
+    @staticmethod
+    def router_base_path_calculator(router_name: str):
+        from .config_manager import ConfigManager
+
+        BASE_PATH = ConfigManager.get_conf_value(
+            config_file_name='api',
+            section='api',
+            value='base_path'
+        )
+        CURRENT_VERSION = ConfigManager.get_conf_value(
+            config_file_name='api',
+            section='api',
+            value=f'current_version'
+        )
+        ROUTER_BASE_PATH = ConfigManager.get_conf_value(
+            config_file_name='api',
+            section='routers',
+            value=f'{router_name}_router_base_path'
+        )
+        return f'{BASE_PATH}{CURRENT_VERSION}/{ROUTER_BASE_PATH}'
