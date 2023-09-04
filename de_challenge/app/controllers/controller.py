37094ed241719154b0e42ca3a3e141e33ee15a01
Bruno Tomás Caracini <bruno98980@gmail.com>
2023-09-04 01:17:45 -0300

    controller for Job - initial version with main controller

--- de_challenge/app/controllers/controller.py
+++ de_challenge/app/controllers/controller.py
@@ -1,14 +1,18 @@
+"-------------------------------Imports Section-------------------------------"
+
+# Libraries
 import io
 import csv
-import pandas as pd
 
 
 class Controller:
-
+    
     @staticmethod
     async def read_csv_reader(file, headers: list[str] = None):
         if not file.filename.lower().endswith(".csv"):
             raise Exception("File is not in CSV format")
         file_content = await file.read()
-        csv_reader = csv.DictReader(io.StringIO(file_content.decode("utf-8")), fieldnames=headers)
+        csv_reader = csv.DictReader(
+            io.StringIO(file_content.decode("utf-8")), fieldnames=headers
+        )
         return [row for row in csv_reader]
--- de_challenge/app/controllers/entities/jobs_controller.py
+++ de_challenge/app/controllers/entities/jobs_controller.py
@@ -1,9 +1,20 @@
+"-------------------------------Imports Section-------------------------------"
+
+# Libraries
+from fastapi.responses import Response
+
+# Local Dependencies
 from app.controllers.controller import Controller
 from app.database.entities.jobs import Job as JobData
 
-class JobController(Controller):
 
+class JobController(Controller):
     @staticmethod
     async def batch_from_csv(file: bytes, headers: list[str]):
         jobs = await JobController.read_csv_reader(file=file, headers=headers)
+        return await JobData.insert_many(jobs=jobs)
+
+    @staticmethod
+    async def insert_many(jobs: list[dict]):
+        jobs = [{**obj.__dict__} for obj in jobs]
         return await JobData.insert_many(jobs=jobs)
