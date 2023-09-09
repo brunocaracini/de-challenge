70f33714a5ade22c3c40437c49b11fe3be771e9b
Bruno Tomás Caracini <bruno98980@gmail.com>
2023-09-09 20:43:56 -0300

    Pipeline build fixed

--- de_challenge/app/config/env_settings.py
+++ de_challenge/app/config/env_settings.py
@@ -1,9 +1,9 @@
 import os
 
 if not os.getenv('ENVIRONMENT', False):
+    print('sono qui')
     from dotenv import load_dotenv
     load_dotenv('/.env',)
-
 
 #Database env variables
 DB_HOST = os.environ.get("DB_HOST")
@@ -11,3 +11,5 @@ DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("POSTGRES_DB")
 DB_USER = os.environ.get("POSTGRES_USER")
 DB_PASSWORD = os.environ.get("POSTGRES_PASSWORD")
+
+print(DB_HOST, DB_PORT, DB_PASSWORD, DB_NAME, DB_USER)
