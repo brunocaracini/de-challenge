b586d3d6dd0c96d3b9fc5d87074dbb8b910ef5aa
Bruno Tomás Caracini <bruno98980@gmail.com>
2023-09-10 18:09:32 -0300

    Host for IP of the db added

--- de_challenge/app/config/env_settings.py
+++ de_challenge/app/config/env_settings.py
@@ -1,12 +1,18 @@
 import os
+import socket
 
 if not os.getenv('ENVIRONMENT', False):
     print('sono qui')
+    # Use socket.gethostbyname to resolve the hostname to an IP address
+    db_host = socket.gethostbyname("db")
+
+    # You can print the resolved IP address for debugging purposes
+    print(f"Resolved DB host IP: {db_host}")
     from dotenv import load_dotenv
     load_dotenv('/.env',)
 
 #Database env variables
-DB_HOST = os.environ.get("DB_HOST")
+DB_HOST = socket.gethostbyname("db")
 DB_PORT = os.environ.get("DB_PORT")
 DB_NAME = os.environ.get("POSTGRES_DB")
 DB_USER = os.environ.get("POSTGRES_USER")
