90e2806943697e35f6a83336e39de550bc4c8633
Bruno Tomás Caracini <bruno98980@gmail.com>
2023-09-11 00:59:53 -0300

    static methods converted into classmethods on db entities

--- de_challenge/app/database/entities/hired_employees.py
+++ de_challenge/app/database/entities/hired_employees.py
@@ -28,73 +28,71 @@ class HiredEmployee(BaseDBModel, BASE):
     department_id = Column(Integer, ForeignKey("departments.id"))
     job_id = Column(Integer, ForeignKey("jobs.id"))
 
-    @staticmethod
-    def __delete_table__():
-        BASE.metadata.tables["hired_employees"].drop(ENGINE)
+    @classmethod
+    def delete_table(cls):
+        cls.metadata.drop_all(ENGINE)
 
-    @staticmethod
-    def __create_table__():
-        BASE.metadata.create_all(ENGINE)
+    @classmethod
+    def __create_table__(cls):
+        cls.metadata.create_all(ENGINE)
 
-    @staticmethod
-    async def insert_many(hired_employees: list[dict]):
-        return await BaseDBModel.insert_many(
-            entities=hired_employees, model_class=HiredEmployee
-        )
+    @classmethod
+    async def insert_many(cls, hired_employees: list[dict]):
+        return await super().insert_many(entities=hired_employees, model_class=cls)
 
-    @staticmethod
-    async def get_by_job_and_department(year: int):
-        session = BaseDBModel.session_factory()
-        quarter = extract("quarter", HiredEmployee.datetime)
+    @classmethod
+    async def get_by_job_and_department(cls, year: int):
+        with super().session_factory() as session:
+            quarter = extract("quarter", HiredEmployee.datetime)
 
-        query = (
-            session.query(
-                Department.department.label("Department"),
-                Job.job.label("Job"),
-                func.sum(case((quarter == 1, 1), else_=0)).label("Q1"),
-                func.sum(case((quarter == 2, 1), else_=0)).label("Q2"),
-                func.sum(case((quarter == 3, 1), else_=0)).label("Q3"),
-                func.sum(case((quarter == 4, 1), else_=0)).label("Q4"),
+            query = (
+                session.query(
+                    Department.department.label("Department"),
+                    Job.job.label("Job"),
+                    func.sum(case((quarter == 1, 1), else_=0)).label("Q1"),
+                    func.sum(case((quarter == 2, 1), else_=0)).label("Q2"),
+                    func.sum(case((quarter == 3, 1), else_=0)).label("Q3"),
+                    func.sum(case((quarter == 4, 1), else_=0)).label("Q4"),
+                )
+                .join(Department, cls.department_id == Department.id)
+                .join(Job, cls.job_id == Job.id)
+                .filter(extract("year", cls.datetime) == year)
+                .group_by(Department.department, Job.job)
+                .order_by(Department.department, Job.job)
             )
-            .join(Department, HiredEmployee.department_id == Department.id)
-            .join(Job, HiredEmployee.job_id == Job.id)
-            .filter(extract("year", HiredEmployee.datetime) == year)
-            .group_by(Department.department, Job.job)
-            .order_by(Department.department, Job.job)
-        )
-        return [row._mapping for row in query.all()]
+            return [row._mapping for row in query.all()]
 
-    @staticmethod
-    async def get_by_department_higher_than_year_mean(year: int):
-        session = BaseDBModel.session_factory()
-        subquery = (
-            session.query(
-                Department.id.label("department_id"),
-                func.count(HiredEmployee.id).label("count"),
+    @classmethod
+    async def get_by_department_higher_than_year_mean(cls, year: int):
+        with super().session_factory() as session:
+            subquery = (
+                session.query(
+                    Department.id.label("department_id"),
+                    func.count(cls.id).label("count"),
+                )
+                .join(cls, Department.id == cls.department_id)
+                .filter(func.extract("year", cls.datetime) == year)
+                .group_by(Department.id)
+                .subquery()
             )
-            .join(HiredEmployee, Department.id == HiredEmployee.department_id)
-            .filter(func.extract("year", HiredEmployee.datetime) == year)
-            .group_by(Department.id)
-            .subquery()
-        )
 
-        # Main query to retrieve department information with hired employee counts
-        query = (
-            session.query(
-                Department.id,
-                Department.department,
-                func.count(HiredEmployee.id).label("hired"),
+            # Main query to retrieve department information with hired employee counts
+            query = (
+                session.query(
+                    Department.id,
+                    Department.department,
+                    func.count(cls.id).label("hired"),
+                )
+                .join(cls, Department.id == cls.department_id)
+                .filter(func.extract("year", cls.datetime) == year)
+                .group_by(Department.id, Department.department)
+                .having(
+                    func.count(cls.id)
+                    > session.query(
+                        func.avg(subquery.c.count).label("mean_employees_hired")
+                    ).scalar()
+                )
+                .order_by(func.count(cls.id).desc())
             )
-            .join(HiredEmployee, Department.id == HiredEmployee.department_id)
-            .filter(func.extract("year", HiredEmployee.datetime) == year)
-            .group_by(Department.id, Department.department)
-            .having(
-                func.count(HiredEmployee.id)
-                > session.query(
-                    func.avg(subquery.c.count).label("mean_employees_hired")
-                ).scalar()
-            )
-            .order_by(func.count(HiredEmployee.id).desc())
-        )
 
-        return [row._mapping for row in query.all()]
+            return [row._mapping for row in query.all()]
--- de_challenge/app/database/entities/jobs.py
+++ de_challenge/app/database/entities/jobs.py
@@ -13,14 +13,14 @@ class Job(BaseDBModel, BASE):
     id = Column(Integer, primary_key=True, autoincrement=True)
     job = Column(String, unique=True)
 
-    @staticmethod
-    def __delete_table__():
-        BASE.metadata.tables["jobs"].drop(ENGINE)
+    @classmethod
+    def delete_table(cls):
+        cls.metadata.drop_all(ENGINE)
 
-    @staticmethod
-    def __create_table__():
-        BASE.metadata.create_all(ENGINE)
+    @classmethod
+    def __create_table__(cls):
+        cls.metadata.create_all(ENGINE)
 
-    @staticmethod
-    async def insert_many(jobs):
-        return await BaseDBModel.insert_many(entities=jobs, model_class=Job)
+    @classmethod
+    async def insert_many(cls, jobs):
+        return await super().insert_many(entities=jobs, model_class=cls)
